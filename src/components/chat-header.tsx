34ddefe5694e238248e5c9b48debc60928685dbf
greff379 <igor.greff@yandex.ru>
2024-07-24 00:03:08 +0300

    Update chat-header.tsx

--- src/components/chat-header.tsx
+++ src/components/chat-header.tsx
@@ -5,7 +5,7 @@ export function ChatHeader() {
   return (
     <div className="flex flex-col items-center justify-center">
       <Image alt="logo" src={LogoIcon} width={60}/>
-      <div className="mt-4 mb-8 font-bold header-title">Bing is an AI-powered online assistant</div>
+      <div className="mt-4 mb-8 font-bold header-title">GPT-ChatBot.ru is an AI-powered online assistant</div>
     </div>
   )
 }

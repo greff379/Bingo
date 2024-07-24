f6a9313d89b042bb4015816e70dfdacd402e954c
greff379 <igor.greff@yandex.ru>
2024-07-24 11:25:01 +0300

    Update chat-header.tsx

--- src/components/chat-header.tsx
+++ src/components/chat-header.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 export function ChatHeader() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <Image alt="logo" src={LogoIcon} width={60}/>
-      <div className="mt-4 mb-8 font-bold header-title">GPT-ChatBot.ru is an AI-powered online assistant</div>
+      <Image alt="logo" src={LogoIcon} width={20}/>
+      <div className="mt-4 mb-8 font-bold header-title"><a href="https://gpt-chatbot.ru/">GPT-ChatBot.ru</a> is an AI-powered online assistant</div>
     </div>
   )
 }
